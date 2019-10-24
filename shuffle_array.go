0a06fa0c51c3f69ce5323cd7bf312a0d00181532
Marcelo Gabriel Milera Sanchez <marcelo.milera@fisglobal.com>
2019-10-23 23:47:59 -0500

    Adding shuffle function

--- shuffle_array.go
+++ shuffle_array.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var numbers = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 func main() {
-	fmt.Println("Hello, 世界")
+	shuffled_array := shuffle(numbers)
+	fmt.Println(shuffled_array)
+}
+
+/* 
+Implementation of the "inside-out" version of the
+Fisher-Yates shuffle algorithm 
+Source: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_%22inside-out%22_algorithm
+*/
+func shuffle(numbers []int) []int {
+	//Initializing the default Source to a deterministic state.
+	rand.Seed(time.Now().UTC().UnixNano())
+	
+	for i := 0; i < len(numbers); i++ {
+		j := rand.Intn(i+1)
+		if j != i {
+			numbers[i], numbers[j] = numbers[j], numbers[i]
+		}
+	}
+	return numbers
 }
